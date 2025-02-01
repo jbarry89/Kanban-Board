529b91d7b9e93408f7e18686ff73f45389b577f1
jbarry89 <joelbarry8907@hotmail.com>
2025-02-01 16:47:09 -0500

    Added code to verify password. If correct, generate JWT token

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -3,8 +3,32 @@ import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
+const SECRET_KEY = process.env.JWT_SECRET_KEY || 'your_secret_key';
+
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  const {username, password} = req.body;
+
+  try {
+    const user = await User.findOne({where: {username}});
+    if(!user){
+      return res.status(401).json({message: 'Invalid user or password'});
+    }
+
+    //Comparing the password with the stored hashed password
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword){
+      return res.status(401).json({message: 'Invalid username or password'});
+    }
+
+    // Generate a JWT token
+    const token = jwt.sign({userId: user.id, username: user.username}, SECRET_KEY, {expiresIn: '1h'});
+    res.json({token});
+  } catch (error: any) {
+    res.status(500).json({message: error.message});
+  }
+
+
 };
 
 const router = Router();
