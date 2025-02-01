719c0c5a01832cc98654975bdb3a84c752c9680a
jbarry89 <joelbarry8907@hotmail.com>
2025-02-01 16:03:13 -0500

    Addedd code to handle token validation.

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -5,6 +5,27 @@ interface JwtPayload {
   username: string;
 }
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  // TODO: verify the token exists and add the user data to the request object
+interface AuthRequest extends Request {
+  user?: JwtPayload;
+}
+
+const SECRET_KEY = process.env.JWT_SECRET_KEY || 'your_secret_key';  // secret key
+
+export const authenticateToken = (req: AuthRequest, res: Response, next: NextFunction) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];  // The token is extracted from the authHeader and split to remove the 'bearer' prefix if it exist.
+  
+  // Verify the token exists and add the user data to the request object
+  if(!token){
+    return res.status(401).json({message: "Access Denied. No Token Provided." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY) as JwtPayload;
+    req.user = decoded;
+    return next();
+  } catch (err) {
+    return res.status(403).json({message: 'Failed to authenticate token'});
+  }
+
 };
