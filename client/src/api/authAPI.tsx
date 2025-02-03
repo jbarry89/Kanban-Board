24fcd03ce74a0a83f5d2f5a4c62a47e935513f06
jbarry89 <joelbarry8907@hotmail.com>
2025-02-03 10:13:03 -0500

    Added code for the POST request to the login route

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,7 +2,29 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
-}
+  try {
+    const response = await fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    });
+
+    const data = await response.json();
+
+    if(response.ok){
+      localStorage.setItem('token', data.token);
+      return {success: true, token: data.token};
+    }
+
+  } catch (error: any){
+    console.error ("Login Failed:", error);
+    return {success: false, message: error.message};
+
+  }
+
+};
 
 
 
