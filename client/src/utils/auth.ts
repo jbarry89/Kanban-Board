5fc4e15571c3e9283df0ee5af99a8c60f7b95e9f
jbarry89 <joelbarry8907@hotmail.com>
2025-02-03 11:15:08 -0500

    Added code for getting token, logging in and out, checking expiration date from token.

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,28 +3,51 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    const token = this.getToken();
+    if (token){
+      return jwtDecode<JwtPayload>(token);
+    }
+    return null;
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    try {
+      const decoded = jwtDecode<JwtPayload>(token);
+      if(!decoded.exp){
+        return true;   // If there is no expiration field
+      }
+      // Comparing expiration date with current date.
+      return decoded.exp * 1000 < Date.now();
+    
+    } catch (error) {
+      return true;  // Token is expired
+    }
   }
 
-  getToken(): string {
+  getToken(): string | null {
     // TODO: return the token
+    return localStorage.getItem('token');
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
+    localStorage.setItem('token', idToken);
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
+    localStorage.removeItem('token');
+    window.location.assign('/login');
   }
 }
 
