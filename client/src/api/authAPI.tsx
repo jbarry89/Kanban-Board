63c1bebf3c3a42efa96ccf6a35c3c8523cf0cdf6
jbarry89 <joelbarry8907@hotmail.com>
2025-02-05 00:21:29 -0500

    Added some code

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,9 +1,11 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
-const login = async (userInfo: UserLogin) => {
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
+
+const login = async (userInfo: UserLogin): Promise<{success: boolean, token?:string, message?:string}> => {
   // TODO: make a POST request to the login route
   try {
-    const response = await fetch('/auth/login', {
+    const response = await fetch(`${API_URL}/auth/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -16,16 +18,15 @@ const login = async (userInfo: UserLogin) => {
     if(response.ok){
       localStorage.setItem('token', data.token);
       return {success: true, token: data.token};
+    } else {
+      throw new Error(data.message || 'Login Failed');
     }
 
   } catch (error: any){
     console.error ("Login Failed:", error);
-    return {success: false, message: error.message};
-
+    return {success: false, message: error.message || 'An Error has occurred'};
   }
 
 };
-
-
 
 export { login };
