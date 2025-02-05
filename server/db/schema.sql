6f2dcdcbf1e626fb61a37c0a964682187bc46d39
jbarry89 <joelbarry8907@hotmail.com>
2025-02-05 00:14:46 -0500

    Added Tables of users and tickets

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -3,3 +3,24 @@ DROP DATABASE IF EXISTS kanban_db;
 
 -- CREATE DATABASE
 CREATE DATABASE kanban_db;
+
+\c kanban_db;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,           
+    username VARCHAR(255) UNIQUE,   
+    password VARCHAR(255),           -- Hashed password
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  -- Automatically set creation time
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP   -- Automatically set last update time
+);
+
+CREATE TABLE tickets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    status VARCHAR(50) NOT NULL, 
+    description TEXT,
+    assignedUserId INTEGER,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    FOREIGN KEY (assignedUserId) REFERENCES users(id)
+);
